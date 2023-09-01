0db7ac3ce896de55531da0198798545cbcd4c0db
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-01 09:04:18 -0300

    Remove trpc

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TrpcModule } from '@providers/trpc/trpc.module';
 import { PrismaModule } from '@providers/prisma/prisma.module';
 import { PokemonModule } from '@models/pokemon/pokemon.module';
 import { TeamModule } from '@models/team/team.module';
@@ -21,7 +20,6 @@ import { ConfigModule } from '@nestjs/config';
     TypesModule,
     NaturesModule,
     ItemsModule,
-    TrpcModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TrpcRouter } from './providers/trpc/trpc.router';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const trpc = app.get(TrpcRouter);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-  trpc.applyMiddleware(app);
 
   await app.listen(process.env.PORT || 3333);
 }
