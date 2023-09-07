b5c49f176f2a617e8a28bcf0ce065a023b805866
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-07 12:39:04 -0300

    Created new ui package

---
+++ packages/ui2/README.md
@@ -0,0 +1,3 @@
+# TODO
+
+- Solve: Fast refresh only works when a file only exports components. Use a new file to share constants or functions between components.eslint(react-refresh/only-export-components)
---
+++ packages/ui2/vite.config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    watch: {},
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'ui2',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        sourcemap: true,
+      },
+    },
+    emptyOutDir: true,
+    outDir: 'dist',
+  },
+  plugins: [react(), dts()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
