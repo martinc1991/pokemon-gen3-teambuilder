973f7dd3d4d5f6b26fd18635036a2e5a3aecaa61
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-12 17:10:20 -0300

    Added dropdown menu component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,6 @@
 import './src/global.css';
 
 export * from './src/components/ui/button';
+export * from './src/components/ui/dropdown-menu';
 export * from './src/components/ui/table';
 export * as Typography from './src/components/ui/typography';
