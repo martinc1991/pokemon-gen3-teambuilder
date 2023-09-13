6e6bd744281e1db1d2f73ed8ac37fc29d6c86fcf
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-13 18:12:11 -0300

    Add substitute placeholder

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,5 +2,6 @@ import './src/global.css';
 
 export * from './src/components/ui/button';
 export * from './src/components/ui/dropdown-menu';
+export * from './src/components/ui/substitute-placeholder';
 export * from './src/components/ui/table';
 export * as Typography from './src/components/ui/typography';
---
+++ packages/ui/src/components/ui/substitute-placeholder.tsx
@@ -0,0 +1,14 @@
+import Image, { ImageProps } from 'next/image';
+import substitute from '../sprites/pokemon/substitute.png';
+
+type SubstitutePlaceholderProps = Omit<ImageProps, 'src' | 'alt'>;
+
+const DEFAULT_HEIGHT = 30;
+
+export function SubstitutePlaceholder({ height = DEFAULT_HEIGHT, ...props }: SubstitutePlaceholderProps) {
+  return (
+    <div className='flex justify-center'>
+      <Image {...props} src={substitute} height={height} width={height} title={'substitute'} alt='substitute' />
+    </div>
+  );
+}
