126646cf216fcc07873f69d5151f10b91bb72345
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-16 11:26:04 -0300

    Customized colums widths

--- apps/web/src/components/pokemon-table/components/table-header.tsx
+++ apps/web/src/components/pokemon-table/components/table-header.tsx
@@ -2,6 +2,7 @@ import type { HeaderGroup, Table } from '@tanstack/react-table';
 import { flexRender } from '@tanstack/react-table';
 import type { IPokemonGetAllResponseElement } from 'contract';
 import { TableHead, TableHeader, TableRow } from 'ui';
+import { ColumnID, columnsConfig } from './constants';
 
 interface PokemonTableHeaderProps {
   table: Table<IPokemonGetAllResponseElement>;
@@ -26,8 +27,11 @@ function PokemonHeaderRow({ headerGroup, key }: PokemonHeaderRowProps): JSX.Elem
   return (
     <TableRow className='flex hover:bg-transparent' key={headerGroup.id}>
       {headerGroup.headers.map((header) => {
+        const ID = header.getContext().header.id as ColumnID; // Had to cast just for typing
+        const colFlexSize = columnsConfig[ID].colFlexSize;
+        const maxWidth = columnsConfig[ID].maxWidth;
         return (
-          <TableHead className='flex items-center justify-center flex-1' key={key}>
+          <TableHead className='flex items-center justify-center' style={{ flex: colFlexSize, maxWidth: maxWidth }} key={key}>
             {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
           </TableHead>
         );
--- apps/web/src/components/pokemon-table/components/table-rows.tsx
+++ apps/web/src/components/pokemon-table/components/table-rows.tsx
@@ -3,6 +3,7 @@ import { flexRender } from '@tanstack/react-table';
 import type { IPokemonGetAllResponseElement } from 'contract';
 import { TableCell, TableRow, Typography } from 'ui';
 import { columns } from './columns';
+import { ColumnID, columnsConfig } from './constants';
 
 interface PokemonRowProps {
   row: Row<IPokemonGetAllResponseElement>;
@@ -17,19 +18,24 @@ export function PokemonTableRow({ row, key, size, start }: PokemonRowProps): JSX
       data-state={row.getIsSelected() && 'selected'}
       key={key}
       style={{
+        // Needed for virtualization
         position: 'absolute',
         top: 0,
         left: 0,
-        width: '100%',
         height: `${size}px`,
         transform: `translateY(${start}px)`,
+        // Needed for virtualization
+        width: '100%',
         display: 'flex',
         alignItems: 'center',
       }}
     >
       {row.getVisibleCells().map((cell) => {
+        const ID = cell.getContext().column.id as ColumnID; // Had to cast just for typing
+        const colFlexSize = columnsConfig[ID].colFlexSize;
+        const maxWidth = columnsConfig[ID].maxWidth;
         return (
-          <TableCell className='flex-1 text-center' key={cell.id}>
+          <TableCell className='flex-1 text-center' key={cell.id} style={{ flex: colFlexSize, maxWidth: maxWidth }}>
             <Typography.P>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Typography.P>
           </TableCell>
         );
