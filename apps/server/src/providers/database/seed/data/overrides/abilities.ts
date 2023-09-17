394cd58ca4e334e5d38914f2fcd2e86b331cccb3
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-17 17:14:10 -0300

    Added overrides script

---
+++ apps/server/src/providers/database/seed/data/overrides/abilities.ts
@@ -0,0 +1,13 @@
+export const overrideAbilitiesData = {
+  jigglypuff: ['cute-charm'],
+  wigglytuff: ['cute-charm'],
+  zapdos: ['pressure'],
+  igglybuff: ['cute-charm'],
+  plusle: ['plus'],
+  minun: ['minus'],
+  kecleon: ['color-change'],
+  feebas: ['swift-swim'],
+  milotic: ['marvel-scale'],
+  duskull: ['levitate'],
+  dusclops: ['pressure'],
+};
