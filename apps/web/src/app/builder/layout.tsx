88d77fe8d272d61bb2af568820d1b4c17cf880b0
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-19 11:54:30 -0300

    Make table wider

--- apps/web/src/app/builder/layout.tsx
+++ apps/web/src/app/builder/layout.tsx
@@ -7,7 +7,7 @@ export const queryClient = new QueryClient();
 export default function BuilderLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className='flex flex-col items-center w-5/6 min-h-screen gap-2 py-2'>{children}</div>
+      <div className='flex flex-col items-center flex-1 w-5/6 min-h-screen gap-2 py-2'>{children}</div>
     </QueryClientProvider>
   );
 }
