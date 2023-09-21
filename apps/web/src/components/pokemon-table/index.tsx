ba38444ccd9a7bbd260f62cb394aab72e1a8b258
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-21 09:24:17 -0300

    Remove 'use client' from builder layout

--- apps/web/src/components/pokemon-table/index.tsx
+++ apps/web/src/components/pokemon-table/index.tsx
@@ -1,54 +1,14 @@
-import { useVirtualizer } from '@tanstack/react-virtual';
-import { useRef } from 'react';
-import { Table, TableBody, Typography } from 'ui';
-import { useWindowSize } from 'usehooks-ts';
-import { BUILDER_PAGE_HEADER_HEIGHT } from '../../app/builder/constants';
-import LoadingState from '../loading-state';
-import { PokemonTableHeader } from './components/table-header';
-import { EmptyRow, PokemonTableRow } from './components/table-rows';
-import { usePokemonTableConfig } from './hooks/usePokemonTableConfig';
-import { usePokemonTableInfo } from './hooks/usePokemonTableInfo';
+'use client';
+
+import { TableContent } from './components/table';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function PokemonTable(): JSX.Element {
-  const tableContainerRef = useRef(null);
-
-  const { height } = useWindowSize();
-  const tableHeigth = height - BUILDER_PAGE_HEADER_HEIGHT - 20;
-
-  const { flatData, isError, fetchStatus, isFetchingNextPage, isLoading } = usePokemonTableInfo();
-
-  const table = usePokemonTableConfig(flatData);
-  const { rows } = table.getRowModel();
-
-  const rowVirtualizer = useVirtualizer({
-    count: table.getRowModel().rows.length,
-    getScrollElement: () => tableContainerRef.current,
-    estimateSize: () => 50,
-    overscan: 10,
-  });
-
-  if ((fetchStatus === 'fetching' && !isFetchingNextPage) || isLoading) {
-    return <LoadingState />;
-  }
-  if (isError) {
-    return <Typography.P>error...</Typography.P>;
-  }
-
   return (
-    <div className='w-11/12 overflow-auto border rounded-md' ref={tableContainerRef} style={{ height: tableHeigth }}>
-      <Table>
-        <PokemonTableHeader table={table} />
-        <TableBody style={{ height: `${rowVirtualizer.getTotalSize()}px`, width: '600px', position: 'relative' }}>
-          {rowVirtualizer.getVirtualItems().length ? (
-            rowVirtualizer.getVirtualItems().map((virtualRow, i, arr) => {
-              const row = rows[virtualRow.index];
-              return <PokemonTableRow row={row} size={virtualRow.size} start={virtualRow.start} />;
-            })
-          ) : (
-            <EmptyRow />
-          )}
-        </TableBody>
-      </Table>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <TableContent />
+    </QueryClientProvider>
   );
 }
