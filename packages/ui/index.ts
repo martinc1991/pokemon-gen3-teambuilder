908dc5141c5a5d793a54c184d0628a43946c7b0f
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-23 11:57:55 -0300

    Add dialog and label components

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -4,6 +4,8 @@ export * from './src/components/ui/badge';
 export * from './src/components/ui/button';
 export * from './src/components/ui/card';
 export * from './src/components/ui/checkbox';
+export * from './src/components/ui/dialog';
+export * from './src/components/ui/label';
 export * from './src/components/ui/dropdown-menu';
 export * from './src/components/ui/input';
 export * from './src/components/ui/pokemon-avatar';
