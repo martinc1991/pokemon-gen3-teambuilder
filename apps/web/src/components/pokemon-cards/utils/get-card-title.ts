4f7041f32cb0d2291670cc34493f944b9a33f642
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-23 11:49:36 -0300

    Fix getCardTitle

--- apps/web/src/components/pokemon-cards/utils/get-card-title.ts
+++ apps/web/src/components/pokemon-cards/utils/get-card-title.ts
@@ -1,9 +1,9 @@
-import type { FilledSlot } from '../../../state/team/helpers';
+import type { FilledSlot } from '../../../state/helpers/helpers';
 import { formatPokemonName } from '../../../utils/pokemon';
 
 export function getCardTitleName({ pokemon, name, order }: FilledSlot): string {
-  if (name) {
-    return `${name} (${order + 1}. ${formatPokemonName(pokemon.name)})`;
+  if (name && name.trim().length > 0) {
+    return `${order + 1}. ${name} (${formatPokemonName(pokemon.name)})`;
   }
   return `${order + 1}. ${formatPokemonName(pokemon.name)}`;
 }
