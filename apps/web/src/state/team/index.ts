b56c4fefff51249d0be8bf6d57ed4cb16bb86edd
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-23 18:18:59 -0300

    Add immer middleware to store

--- apps/web/src/state/team/index.ts
+++ apps/web/src/state/team/index.ts
@@ -1,32 +1,59 @@
-import type { IPokemon } from 'contract';
+import { type IPokemon, type ISlotOrder } from 'contract';
 import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 import type { TeamSlot } from './helpers';
 import { EmptySlot, addokemonToSlot, emptyTeam, getFirstEmptySlotIndex } from './helpers';
 
 interface TeamState {
   slots: TeamSlot[];
-  addSlot: (pokemon: IPokemon) => void;
-  removeSlot: (slot: TeamSlot) => void;
+  selectedSlotIndex: ISlotOrder | null;
 }
 
-export const useTeamStore = create<TeamState>()((set) => ({
-  slots: emptyTeam,
-  addSlot: (pokemon) => {
-    set((state) => {
-      const index = getFirstEmptySlotIndex(state.slots);
-      if (index === null) return state;
-      const newSlot: TeamSlot = { ...state.slots[index], name: '', pokemon, nationalPokedexNumber: pokemon.nationalPokedexNumber };
+interface TeamActions {
+  addSlot: (pokemon: IPokemon) => void;
+  removeSlot: (slot: TeamSlot) => void;
+  setSelectedSlotIndex: (index: ISlotOrder) => void;
+  setSlotFieldValue: <T extends keyof TeamSlot>(slot: TeamSlot, fieldName: T, fieldValue: TeamSlot[T]) => void;
+}
 
-      const newSlots = addokemonToSlot(state.slots, index, newSlot);
-      return { slots: newSlots };
-    });
-  },
-  removeSlot: (slot: TeamSlot) => {
-    set((state) => {
-      if (!slot.pokemon) return state;
-      const newSlots: TeamSlot[] = state.slots.filter((s) => s.slotId !== slot.slotId);
-      newSlots.push(new EmptySlot(5));
-      return { slots: newSlots };
-    });
-  },
-}));
+export const useTeamStore = create(
+  immer<TeamState & TeamActions>((set) => ({
+    slots: emptyTeam,
+    selectedSlotIndex: 0,
+    addSlot: (pokemon) => {
+      set((state) => {
+        const index = getFirstEmptySlotIndex(state.slots);
+        if (index === null) return state;
+        const newSlot: TeamSlot = { ...state.slots[index], name: '', pokemon, nationalPokedexNumber: pokemon.nationalPokedexNumber };
+
+        const newSlots = addokemonToSlot(state.slots, index, newSlot);
+
+        state.slots = newSlots;
+      });
+    },
+    removeSlot: (slot: TeamSlot) => {
+      set((state) => {
+        if (!slot.pokemon) return state;
+        const newSlots: TeamSlot[] = state.slots.filter((s) => s.slotId !== slot.slotId);
+
+        newSlots.push(new EmptySlot(5));
+
+        for (let i = 0; i < newSlots.length; i++) {
+          newSlots[i].order = i as ISlotOrder;
+        }
+
+        state.slots = newSlots;
+      });
+    },
+    setSelectedSlotIndex: (index) => {
+      set((state) => {
+        state.selectedSlotIndex = index;
+      });
+    },
+    setSlotFieldValue: (slot, field, value) => {
+      set((state) => {
+        state.slots[slot.order][field] = value;
+      });
+    },
+  }))
+);
