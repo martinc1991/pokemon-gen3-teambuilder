f3140ecbdc600f66d08906e1923039693aef7be7
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-25 08:21:19 -0300

    Moved capitalize helper

--- apps/web/src/utils/common/index.ts
+++ apps/web/src/utils/common/index.ts
@@ -3,3 +3,8 @@ export const getKeys = Object.keys as <T extends object>(obj: T) => (keyof T)[];
 
 // Typed Object.values
 export const getValues = Object.values as <T extends object>(obj: T) => T[keyof T][];
+
+// Capitalize one string
+export function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
--- apps/web/src/utils/pokemon/get-tier-name.ts
+++ apps/web/src/utils/pokemon/get-tier-name.ts
@@ -1,10 +1,7 @@
 import type { ITier } from 'contract';
-
-function capitalizeWord(word: string): string {
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+import { capitalize } from '../common';
 
 export function getTierText(tier: ITier): string {
-  if (tier === 'uber') return capitalizeWord(tier);
+  if (tier === 'uber') return capitalize(tier);
   return tier.toUpperCase();
 }
