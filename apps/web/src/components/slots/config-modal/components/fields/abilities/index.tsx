e708e28a4bd2225feac421fdcd887c4a2f50dfd7
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-27 10:00:10 -0300

    Minor style fix on ability slot card

--- apps/web/src/components/slots/config-modal/components/fields/abilities/index.tsx
+++ apps/web/src/components/slots/config-modal/components/fields/abilities/index.tsx
@@ -1,9 +1,8 @@
 import type { CompleteAbility } from 'contract/dist/prisma/zod';
 import type { ComboboxItem } from 'ui';
 import { Combobox, Label } from 'ui';
-import type { FilledSlot } from '../../../../../../state/team/helpers';
 import { useTeamStore } from '../../../../../../state/team';
-import { capitalize } from '../../../../../../utils/common';
+import type { FilledSlot } from '../../../../../../state/team/helpers';
 
 interface AbilitiesConfigFieldProps {
   slot: FilledSlot;
@@ -12,7 +11,11 @@ interface AbilitiesConfigFieldProps {
 export default function AbilitiesConfigField({ slot }: AbilitiesConfigFieldProps): JSX.Element {
   const [setSlotFieldValue] = useTeamStore((state) => [state.setSlotFieldValue]);
 
-  const abilitiesData = slot.pokemon.abilities.map((ability) => ({ id: ability.name, label: capitalize(ability.name), payload: ability }));
+  const abilitiesData = slot.pokemon.abilities.map((ability) => ({
+    id: ability.name,
+    label: ability.name.replace('-', ' '),
+    payload: ability,
+  }));
 
   function handleAbilityChange(item: ComboboxItem<CompleteAbility>): void {
     if (item.payload) {
@@ -28,6 +31,7 @@ export default function AbilitiesConfigField({ slot }: AbilitiesConfigFieldProps
       <Combobox
         data={abilitiesData}
         disabled={abilitiesData.length < 2}
+        itemsClassName='capitalize'
         onChange={handleAbilityChange}
         value={abilitiesData.find((ability) => {
           return ability.payload.name === slot.abilityName;
