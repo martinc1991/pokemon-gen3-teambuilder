406787b50622542210e74b13c8cab5fff7962ffb
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-27 11:12:34 -0300

    Created calculateStat fn

--- apps/web/src/utils/pokemon/stats.ts
+++ apps/web/src/utils/pokemon/stats.ts
@@ -1,4 +1,5 @@
-import type { IBaseStats } from 'contract';
+import type { IBaseStats, StatName } from 'contract';
+import type { CompleteNature } from 'contract/dist/prisma/zod';
 import { getValues } from '../common';
 
 export function getShortStatName(statName: keyof IBaseStats): string {
@@ -22,4 +23,30 @@ export function getShortStatName(statName: keyof IBaseStats): string {
 
 export function getTotalBaseStat(stats: IBaseStats): number {
   return getValues(stats).reduce((a, b) => a + b, 0);
+}
+
+interface CalculateStatProps {
+  statName: StatName;
+  base: number;
+  ev: number;
+  iv: number;
+  level: number;
+  nature: Pick<CompleteNature, 'increased' | 'decreased'>;
+}
+
+// See: https://pokemon.fandom.com/wiki/Statistics#Formula
+export function calculateStat({ statName, base, ev, iv, level, nature }: CalculateStatProps): number {
+  if (statName === 'hp') {
+    return Math.floor(0.01 * (2 * base + iv + Math.floor(0.25 * ev)) * level) + level + 10;
+  }
+
+  let natureMultiplier = 1;
+
+  if (statName === nature.decreased) {
+    natureMultiplier = 0.9;
+  } else if (statName === nature.increased) {
+    natureMultiplier = 1.1;
+  }
+
+  return Math.floor(0.01 * (2 * base + iv + Math.floor(0.25 * ev)) * level + 5) * natureMultiplier;
 }
--- packages/contract/src/types/index.ts
+++ packages/contract/src/types/index.ts
@@ -1,7 +1,7 @@
 import { Slot, StatName } from '@prisma/client';
 import { IPokemonGetAllResponseElement } from '../contracts/pokemon-contract';
 
-export { Gender, NatureNames, TypeNames } from '@prisma/client';
+export { Gender, NatureNames, StatName, TypeNames } from '@prisma/client';
 
 export type ITier = 'lc' | 'nfe' | 'nu' | 'ou' | 'pu' | 'publ' | 'uber' | 'uu' | 'uubl';
 export type IPokemon = IPokemonGetAllResponseElement; // This type is in sync with the response of the get all pokemon endpoint
