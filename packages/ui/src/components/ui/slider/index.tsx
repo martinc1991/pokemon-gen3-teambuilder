b755ff0c8f00efaa1d69a81d53d1843b50454cbc
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-28 12:15:32 -0300

    Add hover on slider component

--- packages/ui/src/components/ui/slider/index.tsx
+++ packages/ui/src/components/ui/slider/index.tsx
@@ -4,7 +4,11 @@ import * as React from 'react';
 
 const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>>(
   ({ className, ...props }, ref) => (
-    <SliderPrimitive.Root ref={ref} className={cn('relative flex w-full touch-none select-none items-center', className)} {...props}>
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn('relative flex w-full touch-none select-none items-center hover:cursor-pointer', className)}
+      {...props}
+    >
       <SliderPrimitive.Track className='relative h-1.5 w-full grow overflow-hidden rounded-full bg-border'>
         <SliderPrimitive.Range
           className='absolute h-full bg-primary'
