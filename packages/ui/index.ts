b9ff2836ed956d31eb8c89b6523ee3e1eb2dc86d
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-09-28 12:15:17 -0300

    Add progress component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -12,6 +12,7 @@ export * from './src/components/ui/input';
 export * from './src/components/ui/label';
 export * from './src/components/ui/pokemon-avatar';
 export * from './src/components/ui/pokemon-icon';
+export * from './src/components/ui/progress';
 export * from './src/components/ui/separator';
 export * from './src/components/ui/slider';
 export * from './src/components/ui/substitute-placeholder';
