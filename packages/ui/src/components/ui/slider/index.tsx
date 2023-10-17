ea8d5655d500e15e30df332a3b1d5e557f3ea7fe
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-10-17 16:03:06 -0300

    Fix slider styles

--- packages/ui/src/components/ui/slider/index.tsx
+++ packages/ui/src/components/ui/slider/index.tsx
@@ -10,17 +10,13 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, R
       {...props}
     >
       <SliderPrimitive.Track className='relative h-1.5 w-full grow overflow-hidden rounded-full bg-border'>
-        <SliderPrimitive.Range
-          className='absolute h-full bg-primary'
-          style={{ background: props.disabled ? 'var(--primary-foreground)' : 'var(--primary)' }}
-        />
+        <SliderPrimitive.Range className={cn('absolute h-full bg-primary', props.disabled && 'bg-border')} />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb
-        className='block w-4 h-4 transition-colors border rounded-full shadow border-primary bg-primary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
-        style={{
-          borderColor: props.disabled ? 'var(--border)' : 'var(--primary)',
-          background: props.disabled ? 'var(--border)' : 'var(--primary)',
-        }}
+        className={cn(
+          'block w-4 h-4 transition-colors border rounded-full shadow border-primary bg-primary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+          props.disabled && 'border-border bg-border'
+        )}
       />
     </SliderPrimitive.Root>
   )
