458790fecb7dc9756a5e0b5209c7003f1e8f03c8
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-10-19 16:59:14 -0300

    Fix gub when height was set above max

--- apps/web/src/components/pokemon-table/hooks/use-pokemon-table-height.ts
+++ apps/web/src/components/pokemon-table/hooks/use-pokemon-table-height.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useWindowSize } from 'usehooks-ts';
 import { PAGE_HEADER_HEIGHT } from '../../page-header';
 import { TABLE_HEADER_HEIGHT } from '../components/table-header';
@@ -5,21 +6,30 @@ import { TABLE_ROW_HEIGHT } from '../components/table-rows';
 import { TABLE_FILTERS_HEIGHT } from '../filters';
 
 const TAILWIND_GAP_4 = 16;
-const MIN_HEIGHT = TABLE_HEADER_HEIGHT + TABLE_ROW_HEIGHT;
+const MIN = TABLE_HEADER_HEIGHT + TABLE_ROW_HEIGHT; // No pokemon found
 
 export function usePokemonTableHeight(RENDERED_ROWS: number): number {
+  const [returnedHeight, setReturnedHeight] = useState(MIN);
   const { height } = useWindowSize();
-  let returnedHeight = MIN_HEIGHT;
 
-  // INFO: 20 to account for botton padding
-  const MAX_HEIGHT = height - PAGE_HEADER_HEIGHT - TABLE_FILTERS_HEIGHT - TAILWIND_GAP_4 - 20;
+  useEffect(() => {
+    // INFO: 20 to account for botton padding
+    const MAX = height - PAGE_HEADER_HEIGHT - TABLE_FILTERS_HEIGHT - TAILWIND_GAP_4 - 20;
 
-  const RENDERED_ELEMENTS_HEIGHT = RENDERED_ROWS * TABLE_ROW_HEIGHT;
-  const TABLE_HEIGHT_CALC = RENDERED_ELEMENTS_HEIGHT > MAX_HEIGHT ? MAX_HEIGHT : RENDERED_ELEMENTS_HEIGHT + TABLE_HEADER_HEIGHT;
+    const RENDERED_ELEMENTS_HEIGHT = RENDERED_ROWS * TABLE_ROW_HEIGHT;
 
-  if (MIN_HEIGHT < TABLE_HEIGHT_CALC) {
-    returnedHeight = TABLE_HEIGHT_CALC;
-  }
+    const HEIGHT = RENDERED_ELEMENTS_HEIGHT + TABLE_HEADER_HEIGHT;
+
+    if (HEIGHT > MAX) {
+      setReturnedHeight(MAX);
+      return;
+    }
+    if (HEIGHT < MIN) {
+      setReturnedHeight(MIN);
+      return;
+    }
+    setReturnedHeight(HEIGHT);
+  }, [height, RENDERED_ROWS]);
 
   // INFO: +3 is because there are 3 border (1px width) inside the table that makes the table scrolleable even if all elements are visible
   return returnedHeight + 3;
