6888838cc3a46aa7861fd88dffb8103661cec756
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-10-22 14:02:00 -0300

    eslint and tsconfig for path alias

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['custom/next'],
+  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'import/no-default-export': 'off',
     'jsx-a11y/no-autofocus': 'off',
