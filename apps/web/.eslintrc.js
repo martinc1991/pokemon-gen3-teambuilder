4d501ba06af8599d923a6444aa78de8f0b10bab4
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-10-23 13:18:22 -0300

    eslint config

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,13 +1,24 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'import/no-default-export': 'off',
     'jsx-a11y/no-autofocus': 'off',
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+          },
+        ],
+        'react-hooks/exhaustive-deps': 'off',
+      },
+    },
+  ],
 };
