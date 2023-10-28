1dd7246ca91900642fcd5077c329b3d393a82416
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-10-28 15:30:12 -0300

    Created team card component

---
+++ apps/web/src/components/teams/card/index.tsx
@@ -0,0 +1,42 @@
+import { FilledSlot } from '@state/team/helpers';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, PokemonIcon, Typography } from 'ui';
+
+export interface CardTeam {
+  user: string;
+  name: string;
+  description: string;
+  slots: FilledSlot[];
+}
+
+interface TeamCardProps {
+  team: CardTeam;
+}
+
+export default function TeamCard({ team }: TeamCardProps): JSX.Element {
+  return (
+    <Card className='w-[450px]'>
+      <CardHeader>
+        <CardTitle>{team.name}</CardTitle>
+        <CardDescription>{team.description}</CardDescription>
+      </CardHeader>
+      <CardContent className='flex gap-4'>
+        {team.slots.map((slot) => {
+          return <PokemonIcon key={slot.id} iconUrl={slot.pokemon.icon} />;
+        })}
+      </CardContent>
+      <CardFooter className='flex justify-between h-14'>
+        <div className='flex gap-2'>
+          <Typography.Muted>By:</Typography.Muted>
+          <Typography.Small>{team.user}</Typography.Small>
+        </div>
+        <div className='flex gap-4 '>
+          {/* <Button size='sm' variant='outline'>
+            Details
+          </Button>
+          <Button size='sm'>Copy</Button>
+          <CopyButton /> */}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
