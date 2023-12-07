42909415a316426e52b6bce84dbd1cee519ad235
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-12-07 11:41:16 -0300

    Remove useless IStats definition

--- packages/contract/src/types/index.ts
+++ packages/contract/src/types/index.ts
@@ -1,11 +1,10 @@
-import { StatName, Team } from '@prisma/client';
+import { Team } from '@prisma/client';
 import { IPokemonGetAllResponseElement } from '../contracts/pokemon-contract';
 import { Slot } from '../prisma/zod';
 
 export type EvFieldName = 'evAttack' | 'evDefense' | 'evHp' | 'evSpAttack' | 'evSpDefense' | 'evSpeed';
 export type IvFieldName = 'ivAttack' | 'ivDefense' | 'ivHp' | 'ivSpAttack' | 'ivSpDefense' | 'ivSpeed';
 export type IPokemon = IPokemonGetAllResponseElement; // This type is in sync with the response of the get all pokemon endpoint
-export type IStats = { [key in StatName]: number };
 export type IBaseStats = {
   baseHp: number;
   baseAttack: number;
