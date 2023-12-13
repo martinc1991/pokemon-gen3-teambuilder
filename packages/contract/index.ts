dfd9ac5a0bc1202e47f7ffdf8b973f08983cdb3a
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-12-13 17:05:16 -0300

    Extract main index file into package root

---
+++ packages/contract/index.ts
@@ -0,0 +1,4 @@
+export * from '@prisma/client';
+export * from './src/constants';
+export * from './src/contracts';
+export * from './src/types';
--- packages/contract/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@prisma/client';
-export * from './constants';
-export * from './contracts';
-export * from './types';
