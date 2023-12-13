93956eb18da70591e60110277ecdcd4293520cfc
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-12-13 15:54:58 -0300

    Create barrell file for contracts folder

---
+++ packages/contract/src/contracts/index.ts
@@ -0,0 +1,6 @@
+export * from './items-contract';
+export * from './main-contract';
+export * from './natures-contract';
+export * from './pokemon-contract';
+export * from './teams-contract';
+export * from './types-contract';
