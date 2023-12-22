05b09942901790af0683e47cc167c5d9e2230e4b
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-12-22 09:31:25 -0300

    Remove unnecessary metadata export

--- apps/web/src/app/types/page.tsx
+++ apps/web/src/app/types/page.tsx
@@ -4,16 +4,11 @@ import PageHeader from '@components/page-header';
 import { TypesChart } from '@components/types/types-chart';
 import { queryClient } from '@rq-client/index';
 import { QueryClientProvider } from '@tanstack/react-query';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Pokemon Gen 3 TeamBuilder',
-};
 
 export default function Types(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <PageHeader description='Types chart.' title='Types' />
+      <PageHeader description='Types chart. Hold over a multiplier for detailed explanation.' title='Types' />
       <div className='flex flex-1 w-full justify-center items-center'>
         {/* TODO: add ofensive and defensive type texts */}
         {/* TODO: add filters */}
