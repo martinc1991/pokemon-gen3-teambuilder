657e68ca8581fb058ef98846998280b25b32d7b1
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-12-24 08:14:44 -0300

    Extract types-filter component handlers

--- apps/web/src/components/pokemon-table/filters/index.tsx
+++ apps/web/src/components/pokemon-table/filters/index.tsx
@@ -1,5 +1,8 @@
+import { client } from '@rq-client/index';
 import type { Table } from '@tanstack/react-table';
-import type { PokemonWithAbilities } from 'contract';
+import type { PokemonWithAbilities, Type } from 'contract';
+import { ComboboxItem } from 'ui';
+import { ColumnID } from '../columns/constants';
 import { AbilitiesFilter } from './abilities-filter';
 import { NameFilter } from './name-filter';
 import { TierFilter } from './tier-filter';
@@ -12,12 +15,28 @@ interface FilterProps {
 }
 
 export function Filters({ table }: FilterProps): JSX.Element {
+  const { isFetching, isError, data } = client.types.getAll.useQuery(['get-all-types']);
+
+  function handleTypeChange(selectedTypes: ComboboxItem<Type>[]): void {
+    table.getColumn(ColumnID.TYPES)?.setFilterValue(selectedTypes.map((type) => type.payload.name));
+  }
+  function handleTypeClear(): void {
+    table.getColumn(ColumnID.TYPES)?.setFilterValue([]);
+  }
+
   return (
     <div className='flex flex-row items-center gap-4' style={{ height: TABLE_FILTERS_HEIGHT }}>
       <TierFilter table={table} />
       <NameFilter table={table} />
       <AbilitiesFilter table={table} />
-      <TypesFilter table={table} />
+      <TypesFilter
+        // TODO: make this not undefined
+        types={data?.body || []}
+        onChange={handleTypeChange}
+        onRemove={handleTypeChange}
+        onClear={handleTypeClear}
+        disabled={isFetching || isError}
+      />
     </div>
   );
 }
