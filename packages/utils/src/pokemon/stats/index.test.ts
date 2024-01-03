7783fd77caa5b27a7de391d5d6b0a3b4657f5757
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-01-03 16:47:55 -0300

    100% coverage on utils package

--- packages/utils/src/pokemon/stats/index.test.ts
+++ packages/utils/src/pokemon/stats/index.test.ts
@@ -19,6 +19,23 @@ describe('getShortStatName', () => {
     expect(getShortStatName('baseSpdefense')).toBe('SpD');
     expect(getShortStatName('baseSpeed')).toBe('Spe');
   });
+  test('should return a hyphen (-) in any other case', () => {
+    const expectedResult = '-';
+    // @ts-expect-error - testing invalid input
+    expect(getShortStatName('')).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName(0)).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName(6)).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName(undefined)).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName(null)).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName({})).toBe(expectedResult);
+    // @ts-ignore - testing invalid input
+    expect(getShortStatName([])).toBe(expectedResult);
+  });
 });
 
 describe('getTotalBaseStat', () => {
@@ -37,16 +54,36 @@ describe('getTotalBaseStat', () => {
 });
 
 describe('calculateStat', () => {
-  test('should return the correct stat value', () => {
-    const input: CalculateStatProps = {
-      statName: 'attack',
-      base: 50,
-      ev: 100,
-      iv: 15,
-      level: 50,
-      nature: { decreased: 'defense', increased: 'attack' },
-    };
+  const defaultInput: CalculateStatProps = {
+    statName: 'attack',
+    base: 50,
+    ev: 100,
+    iv: 15,
+    level: 50,
+    nature: { decreased: 'defense', increased: 'attack' },
+  };
+  test('should return the correct stat value for hp', () => {
+    const input = { ...defaultInput, statName: StatName.hp };
+    const expectedResult = 130;
+    expect(calculateStat(input)).toBe(expectedResult);
+  });
+  test('should return the correct stat value other stats than hp', () => {
     const expectedResult = 82;
+    expect(calculateStat(defaultInput)).toBe(expectedResult);
+  });
+  test('should return the correct stat value when nature is increased', () => {
+    const expectedResult = 82;
+    const input = { ...defaultInput };
+    expect(calculateStat(input)).toBe(expectedResult);
+  });
+  test('should return the correct stat value when nature is neutral', () => {
+    const expectedResult = 75;
+    const input = { ...defaultInput, nature: { decreased: null, increased: null } };
+    expect(calculateStat(input)).toBe(expectedResult);
+  });
+  test('should return the correct stat value when nature is decreased', () => {
+    const expectedResult = 67;
+    const input = { ...defaultInput, nature: { decreased: StatName.attack, increased: null } };
     expect(calculateStat(input)).toBe(expectedResult);
   });
 });
